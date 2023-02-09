5ca4734d88e5d2eae926fba9277fdafa50f19f47
Mario Garcia <mandres_garcia@hotmail.com>
2023-02-09 16:44:38 -0500

    First version

---
+++ src/Modules/Constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#_*_ codig: utf8 _*_
+#source_Path="/mnt/CABLEOPERADORES/VODTOOLBOX_COMP_VCMBOG"
+source_Path="//evo8/Aspera/CABLEOPERADORES/VODTOOLBOX_COMP_VCMBOG" #Test 
+Bucket="bucket-in-companion-tbx"

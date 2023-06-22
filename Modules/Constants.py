f3e01936b24413d368583fc72786b343acfce3a5
Mario Garcia <mandres_garcia@hotmail.com>
2023-06-22 11:09:37 -0500

    Cambio de ruta fuente de contenido

--- Modules/Constants.py
+++ Modules/Constants.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 #_*_ codig: utf8 _*_
-#source_Path="/mnt/CABLEOPERADORES/VODTOOLBOX_COMP_VCMBOG"
-source_Path="//evo8/Aspera/CABLEOPERADORES/VODTOOLBOX_COMP_VCMBOG" #Test 
+source_Path="/mnt/Aspera/CABLEOPERADORES/VODTOOLBOX_COMP_VCMBOG"
 Bucket="bucket-in-companion-tbx"
